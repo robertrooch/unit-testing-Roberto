d5e6ab927291c11f6d55941add9a82a6ece6d1a4
robertrooch <robertorooch2004@gmail.com>
2024-10-15 12:30:23 -0600

    update calcularMedia.ts

--- src/app/media/calcularMedia.ts
+++ src/app/media/calcularMedia.ts
@@ -1,6 +1,7 @@
 export function calcularMedia(valores: number[]): number {
     const suma = valores.reduce((a, b) => a + b, 0);
     const media = suma / valores.length;
+
     return +media.toFixed(2);
   }
   
