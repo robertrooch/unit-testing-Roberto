7edc0261cf95ac0d605cf96ec246785964f28613
robertrooch <robertorooch2004@gmail.com>
2024-10-16 18:34:27 -0600

    add correlation branch #14

---
+++ src/app/correlation/correlation.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-correlation',
+  standalone: true,
+  imports: [],
+  templateUrl: './correlation.component.html',
+  styleUrl: './correlation.component.css'
+})
+export class CorrelationComponent {
+
+}
